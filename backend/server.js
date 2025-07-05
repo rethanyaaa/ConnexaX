8843bb71fb6ace9a56465c4f0743d4be84b30d2f
Rethanya <rethanya318@gmail.com>
2025-07-05 10:07:33 +0530

    setup express

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import cors from "cors";
+ 
+
+dotenv.config();
+const app = express();
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+// app.use(express.json({limit: "40kb"}));
+// app.use(express.urlencoded({limit: "40kb", extended: true}))
+
+
+const start = async () => {
+  app.set("mongo_user");
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://rethanya888:vHn6fzjH5qClw4Kx@cluster0.zex9c.mongodb.net/ConnexaX?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+  app.listen(app.get("port"), () => {
+    console.log("Listening on port 8000");
+  });
+};
+
+start();
